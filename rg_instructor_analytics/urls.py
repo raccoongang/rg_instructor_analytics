fac26e3db81e4e27351ed86b277659499d5b488c
Yura Braiko <yura.braiko@raccoongang.com>
2017-10-23 13:18:25 +0300

    update url

--- rg_instructor_analytics/urls.py
+++ rg_instructor_analytics/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import patterns, url
 from .views import CalendarTabFragmentView
 
 urlpatterns = patterns(
-    'rg_instructor_analytics.views',
+    '',
     url(
         r'^$',
         CalendarTabFragmentView.as_view(),
