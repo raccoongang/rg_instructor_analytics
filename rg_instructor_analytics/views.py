f9e7735fc9b6e5f3daad44b5ee0fa3c258ceeea2
Yura Braiko <yura.braiko@raccoongang.com>
2017-10-23 12:22:28 +0300

    clean code

--- rg_instructor_analytics/views.py
+++ rg_instructor_analytics/views.py
@@ -1,22 +1,14 @@
 import logging
 
-from django.http.response import HttpResponse
-from django.utils.translation import ugettext_noop
-from django.views.decorators.cache import cache_control
-from django.views.decorators.csrf import ensure_csrf_cookie
 from openedx.core.djangoapps.plugin_api.views import EdxFragmentView
 
-from edxmako.shortcuts import render_to_response, render_to_string
+from edxmako.shortcuts import  render_to_string
 from django.http import Http404, HttpResponseServerError
-from courseware.courses import get_course_by_id, get_studio_url
+from courseware.courses import get_course_by_id
 
-from xmodule.tabs import CourseTab
-from courseware.access import has_access
 from opaque_keys.edx.keys import CourseKey
 from opaque_keys import InvalidKeyError
-from web_fragments.views import FragmentView
 from web_fragments.fragment import Fragment
-from . import VENDOR_CSS_URL, VENDOR_JS_URL, VENDOR_PLUGIN_JS_URL, JS_URL
 
 log = logging.getLogger(__name__)
 
