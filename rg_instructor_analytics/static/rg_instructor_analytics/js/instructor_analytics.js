8fbbf2dcc3732cc85b3eca3b6810eac8116eaf8a
Yura Braiko <yura.braiko@raccoongang.com>
2017-11-28 11:10:20 +0200

    add new tab

--- rg_instructor_analytics/static/rg_instructor_analytics/js/instructor_analytics.js
+++ rg_instructor_analytics/static/rg_instructor_analytics/js/instructor_analytics.js
@@ -208,12 +208,354 @@
      */
     function ProblemTab(button, content) {
         var problemTab = new Tab(button, content);
+        var problemDetail = problemTab.content.find("#problem-body");
+
+        /**
+         * function for display general plot with homework`s stat
+         */
+        function updateHomeWork() {
+            function onSuccess(response) {
+
+                const correct_answer = {
+                    x: response.names,
+                    y: response.correct_answer,
+                    data: response.problems,
+                    name: django.gettext('Percent of the correct answers'),
+                    type: 'bar'
+                };
+
+                const attempts = {
+                    x: response.names,
+                    y: response.attempts,
+                    data: response.problems,
+                    name: django.gettext('Average count of attempts'),
+                    type: 'bar'
+                };
+                const data = [correct_answer, attempts];
+
+                Plotly.newPlot('problem-homeworks-stats-plot', data);
+                document.getElementById("problem-homeworks-stats-plot").on('plotly_click', function (data) {
+                    loadHomeWorkProblems(response.problems[data.points[0].pointNumber]);
+                });
+            }
+
+            function onError() {
+                alert("Can not load statistic fo select course");
+            }
+
+            $.ajax({
+                traditional: true,
+                type: "POST",
+                url: "api/problem_statics/homework/",
+                data: {},
+                success: onSuccess,
+                error: onError,
+                dataType: "json"
+            });
+        }
+
+        /**
+         * function for display plot with homework problem stat
+         * @param homeworsProblem string id of problem
+         */
+        function loadHomeWorkProblems(homeworsProblem) {
+            function onSuccess(response) {
+
+                const incorrect = {
+                    y: response.incorrect,
+                    name: django.gettext('Incorrect answers'),
+                    type: 'bar'
+                };
+
+                const correct = {
+                    y: response.correct,
+                    name: django.gettext('Correct answers'),
+                    type: 'bar'
+                };
+                const data = [correct, incorrect];
+
+                const layout = {
+                    barmode: 'relative',
+                    xaxis: {dtick: 1},
+                    yaxis: {dtick: 1}
+                };
+
+                Plotly.newPlot('problems-stats-plot', data, layout);
+
+                document.getElementById("problems-stats-plot").on('plotly_click', function (data) {
+                    displayProblemView(homeworsProblem[data.points[0].pointNumber]);
+                });
+            }
+
+            function onError() {
+                alert("Can not load statistic fo select course");
+            }
+
+            $.ajax({
+                traditional: true,
+                type: "POST",
+                url: "api/problem_statics/homeworksproblems/",
+                data: {problems: homeworsProblem},
+                success: onSuccess,
+                error: onError,
+                dataType: "json"
+            });
+
+        }
+
+        /**
+         * function for render problem body
+         * @param stringProblemID string problem id
+         */
+        function displayProblemView(stringProblemID) {
+            function onSuccess(response) {
+                problemDetail.html(response.html);
+                let problemsWrapper = problemDetail.find(".problems-wrapper");
+                let problemBody = problemsWrapper.attr("data-content");
+                problemsWrapper.html(problemBody);
+
+                //hide problem debug info
+                problemsWrapper.find('.status-icon').hide();
+                problemsWrapper.find('.submit').hide();
+                problemDetail.find('.instructor-info-action').hide();
+
+                bindPlotsPopupForProblem(problemsWrapper, stringProblemID)
+            }
+
+            function onError() {
+                alert("Can not load statistic fo select course");
+            }
+
+            $.ajax({
+                traditional: true,
+                type: "POST",
+                url: "api/problem_statics/problem_detail/",
+                data: {problem: stringProblemID},
+                success: onSuccess,
+                error: onError,
+                dataType: "json"
+            });
+        }
+
         problemTab.loadTabData = function () {
-            console.log("Called loadTabData method for ProblemTab. " +
-                "ProblemTab functionality will be implemented in feature")
+            updateHomeWork()
         };
+
+        problemTab.content.find('.close').click(item => problemTab.content.find('#model_plot').hide());
+
         return problemTab;
     }
+
+    /**
+     * add buttons for show plot for problem`s question
+     * @param problem div with problem`s render
+     * @param stringProblemID string problem id
+     */
+    function bindPlotsPopupForProblem(problem, stringProblemID) {
+        var avalivleQuestions = [OpetionQuestion, RadioOpetionQuestion,MultyChoseQuestion];
+        var questionsDivs = problem.find(".wrapper-problem-response");
+        questionsDivs.each(function (index) {
+            for (var i = 0; i < avalivleQuestions.length; i++) {
+                var html = $(questionsDivs[index]);
+                var question = avalivleQuestions[i](html, stringProblemID);
+                if (question.isCanParse()) {
+                    question.applyToCurrentProblem(html);
+                    break;
+                }
+            }
+        });
+    }
+
+    /**
+     * Base class for questions
+     * @param questionHtml layout with question
+     * @param stringProblemID string id of question`s problem
+     * @class old realisation
+     * @abstract
+     */
+    function BaseQuestion(questionHtml, stringProblemID) {
+        this.questionHtml = questionHtml;
+        this.problemID = stringProblemID;
+
+        /**
+         * abstract method for indicate, can instance of question correct parse given html
+         * @abstract
+         */
+        this.isCanParse = function () {
+            throw new Error("missing implementation")
+        };
+
+        /**
+         * abstract method, that provide information for request about question statistic
+         * @return object with next body:
+         * return {
+         *       type: string // string marker for given question
+         *       questionID: string, // id of question
+         *       answerMap: JSON string // json with relation between real data and it representation in database
+         *  }
+         *
+         * @abstract
+         */
+        this.getRequestMap = function () {
+            throw new Error("missing implementation")
+        };
+
+        /**
+         * function for display server response as bar plot
+         * @param data server response. Object with key - name of position and value - value of position
+         */
+        this.displayBar = function (data) {
+            const plot_popupp = $('#model_plot');
+            plot_popupp.show();
+
+            var x = [];
+            var y = [];
+            Object.keys(data).forEach(function (key) {
+                y.push(key);
+                x.push(data[key]);
+            });
+            const answers = {
+                x: x,
+                y: y,
+                type: 'bar',
+                orientation: 'h'
+            };
+            const layout = {
+                margin: {
+                    l: 500
+                }
+            };
+            Plotly.newPlot('proble-question-plot', [answers],layout);
+        };
+
+        /**
+         * call`s when receive response with stats for question
+         * @param response
+         */
+        this.onGettingStat = function (response) {
+            switch (response.type) {
+                case 'bar':
+                    this.displayBar(response.stats)
+                    break;
+            }
+        };
+
+        this.onGettingStatFail = function () {
+            console.log("something bad headings")
+        };
+
+        /**
+         * apply given instance to some question of problem
+         * @param html layout for inserting button for display plot
+         */
+        this.applyToCurrentProblem = function (html) {
+            const $plotBtn = $('<button id="show-plot">Show plot!</button>');
+            $plotBtn.appendTo(html);
+            $plotBtn.click((item) => {
+                var requestMap = this.getRequestMap();
+                requestMap.problemID = this.problemID;
+                $.ajax({
+                    traditional: true,
+                    type: "POST",
+                    url: "api/problem_statics/problem_question_stat/",
+                    data: requestMap,
+                    success: (response) => this.onGettingStat(response),
+                    error: () => this.onGettingStatFail(),
+                    dataType: "json"
+                });
+            });
+        };
+    }
+
+    /**
+     * Implementation for the drop down question
+     * @param questionHtml
+     * @param stringProblemID
+     * @return {OpetionQuestion}
+     * @class old realisation
+     * @extends BaseQuestion
+     */
+    function OpetionQuestion(questionHtml, stringProblemID) {
+        const optionQuestion = new BaseQuestion(questionHtml, stringProblemID);
+        const question = optionQuestion.questionHtml.find('.option-input');
+        optionQuestion.questionHtml = question.length === 1 ? $(question[0]) : null;
+
+        optionQuestion.isCanParse = function () {
+            return this.questionHtml !== null
+        };
+
+        optionQuestion.getRequestMap = function () {
+            const selectRoot = this.questionHtml.find('select');
+            const optionsItem = selectRoot.find('option');
+            const id2ValueMap = {};
+            optionsItem.each(index => id2ValueMap[optionsItem[index].value] = optionsItem[index].text);
+            return {
+                type: 'select',
+                questionID: selectRoot.attr('name').replace('input_', ''),
+                answerMap: JSON.stringify(id2ValueMap)
+            }
+
+        };
+
+        return optionQuestion;
+    }
+
+    /**
+     * Implementation for the single choice question
+     * @param questionHtml
+     * @param stringProblemID
+     * @return {RadioOpetionQuestion}
+     * @class old realisation
+     * @extends BaseQuestion
+     */
+    function RadioOpetionQuestion(questionHtml, stringProblemID) {
+        const optionQuestion = new BaseQuestion(questionHtml, stringProblemID);
+        const question = optionQuestion.questionHtml.find('.choicegroup');
+        optionQuestion.questionHtml = question.length === 1 ? $(question[0]) : null;
+        optionQuestion.options = optionQuestion.questionHtml.find('input[type="radio"]');
+
+        optionQuestion.isCanParse = function () {
+            return optionQuestion.options.length > 0
+        };
+
+        optionQuestion.getRequestMap = function () {
+            const id2ValueMap = {};
+            this.options.each(index =>
+                id2ValueMap[this.options[index].value] = this.options[index].parentNode.childNodes[2].nodeValue.trim());
+            return {
+                type: 'select',
+                questionID: this.questionHtml.attr('id').replace('inputtype_', ''),
+                answerMap: JSON.stringify(id2ValueMap)
+            }
+
+        };
+
+        return optionQuestion;
+    }
+
+
+    /**
+     * Implementation for the question with checkbox
+     * @param questionHtml
+     * @param stringProblemID
+     * @return {MultyChoseQuestion}
+     * @class old realisation
+     * @extends RadioOpetionQuestion
+     */
+    function MultyChoseQuestion(questionHtml, stringProblemID) {
+        const multyChooseQuestion = new RadioOpetionQuestion(questionHtml, stringProblemID);
+        multyChooseQuestion.options = multyChooseQuestion.questionHtml.find('input[type="checkbox"]');
+
+        var baseRequestMapFunction  = multyChooseQuestion.getRequestMap;
+        multyChooseQuestion.getRequestMap = function () {
+            var result =  baseRequestMapFunction.call(this)
+            result.type = 'multySelect';
+            return result;
+        };
+
+        return multyChooseQuestion;
+    }
+
 
     /**
      * Implementation of Tab for gradebook tab
@@ -256,5 +598,9 @@
 
         toggleToTab(tabList[0]);
     });
+
+    window.setup_debug = function (element_id, edit_link, staff_context) {
+        // stub function.
+    };
 
 }).call(this);
--- rg_instructor_analytics/urls.py
+++ rg_instructor_analytics/urls.py
@@ -3,9 +3,19 @@ Url config file.
 """
 from django.conf.urls import url
 
-from rg_instructor_analytics.views import EnrollmentStatisticView, InstructorAnalyticsFragmentView
+from rg_instructor_analytics.views import (
+    EnrollmentStatisticView, InstructorAnalyticsFragmentView, ProblemDetailView, ProblemHomeWorkStatisticView,
+    ProblemQuestionView, ProblemsStatisticView)
 
 urlpatterns = [
     url(r'^api/enroll_statics/$', EnrollmentStatisticView.as_view(), name='enrollment_statistic_view'),
+    url(r'^api/problem_statics/homework/$', ProblemHomeWorkStatisticView.as_view(),
+        name='problem_homework_statistic_view'),
+    url(r'^api/problem_statics/homeworksproblems/$', ProblemsStatisticView.as_view(),
+        name='problems_statistic_view'),
+    url(r'^api/problem_statics/problem_detail/$', ProblemDetailView.as_view(),
+        name='problem_detail_view'),
+    url(r'^api/problem_statics/problem_question_stat/$', ProblemQuestionView.as_view(),
+        name='problem_question_view'),
     url(r'^', InstructorAnalyticsFragmentView.as_view(), name='instructor_analytics_dashboard'),
 ]
