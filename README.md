aab3650ed5698e53c6b4012e2c29105442e9e7c6
Yura [flying_pi] Braiko <fp.flyingpi@gmail.com>
2017-12-28 10:42:40 +0200

    add new Problem-tab

--- README.md
+++ README.md
@@ -1,8 +1,18 @@
 # rg_instructor_analytics
 
-##Unit tests
+## Unit tests
 All tests could be run only in local. 
-#####For run unit test follow the next steps:
+##### For run unit test follow the next steps:
 * Ensure that the source placed in one of the edx-platform subdirectory.
 * cd rg_instructor_analytics
 * sh ./test_tool/run_test.sh
+
+## For collect static:
+```bash
+ssh ubuntu@edx-staging-rg-instructor-analytics.raccoongang.com
+sudo -Hu edxapp bash;
+source /edx/app/edxapp/edxapp_env
+cd /edx/app/edxapp/edx-platform
+python ./manage.py lms collectstatic --settings=$EDX_PLATFORM_SETTINGS --noinput
+sudo /edx/bin/supervisorctl restart edxapp:lms
+```
--- rg_instructor_analytics/views.py
+++ rg_instructor_analytics/views.py
@@ -1,14 +1,20 @@
 """
 Module for tab fragment and api views.
 """
+from abc import ABCMeta, abstractmethod
 from datetime import datetime
+from itertools import chain
+import json
 import logging
 from time import mktime
 
 from courseware.access import has_access
 from courseware.courses import get_course_by_id
+from courseware.models import StudentModule
+from courseware.module_render import xblock_view
 from django.conf import settings
-from django.db.models import Count
+from django.db.models import Avg, Count, Q, Sum
+from django.db.models import IntegerField
 from django.db.models.expressions import RawSQL
 from django.db.models.fields import DateField
 from django.http import HttpResponseBadRequest
@@ -21,11 +27,16 @@ from openedx.core.djangoapps.plugin_api.views import EdxFragmentView
 from student.models import CourseEnrollment
 from web_fragments.fragment import Fragment
 
+logging.basicConfig()
 
 log = logging.getLogger(__name__)
+log.setLevel(logging.DEBUG)
 
 JS_URL = '{static_url}rg_instructor_analytics/js/'.format(static_url=settings.STATIC_URL)
 CSS_URL = '{static_url}rg_instructor_analytics/css/'.format(static_url=settings.STATIC_URL)
+
+QUESTUIN_SELECT_TYPE = 'select'
+QUESTUIN_MULTI_SELECT_TYPE = 'multySelect'
 
 
 class EnrollmentStatisticView(View):
@@ -45,6 +56,7 @@ class EnrollmentStatisticView(View):
             CourseEnrollment
             .history
             .filter(course_id=course_key, history_date__lt=date)
+            .filter(~Q(history_type='+'))
             .values('is_active')
             .annotate(count=Count('is_active'))
             .order_by('is_active')
@@ -79,10 +91,12 @@ class EnrollmentStatisticView(View):
             CourseEnrollment
             .history
             .filter(course_id=course_key, history_date__range=(from_date, to_date))
+            .filter(~Q(history_type='+'))
             .annotate(date=RawSQL("select DATE(history_date)", (), output_field=DateField()))
             .values("date", "is_active")
             .annotate(count=Count('date'))
-            .order_by('is_active').order_by('date')
+            .order_by('is_active')
+            .order_by('date')
         )
 
         return enrollment_query
@@ -151,6 +165,235 @@ class EnrollmentStatisticView(View):
         to_timestamp = int(request.POST['to'])
 
         return JsonResponse(data=self.get_statistic_per_day(from_timestamp, to_timestamp, course_key))
+
+
+class ProblemHomeWorkStatisticView(View):
+    """Api for get homework`s statistic for given course."""
+
+    _PARSABLE_PROBLEMS = frozenset(['multiplechoiceresponse', 'choiceresponse', 'stringresponse', 'optionresponse'])
+    _LABEL = 'label'
+    _DESCRIPTION = 'label'
+
+    @staticmethod
+    def academic_performance_request(course_key):
+        """
+        Make request to db for academic performance.
+
+        Return list, where each item contain id of problem, average count of attempts and percent of correct answer.
+        """
+        attempts = RawSQL("SELECT SUBSTRING_INDEX(SUBSTRING_INDEX(state,'attempts\": ',-1),',',1)",
+                          (),
+                          output_field=IntegerField())
+
+        return (StudentModule.objects
+                             .filter(course_id__exact=course_key, grade__isnull=False, module_type__exact="problem")
+                             .values('module_state_key')
+                             .annotate(attempts_avg=Avg(attempts))
+                             .annotate(grade_avg=Sum('grade') / Sum('max_grade'))
+                             .values('module_state_key', 'attempts_avg', 'grade_avg'))
+
+    @classmethod
+    def get_academic_performance(cls, course_key):
+        """Provide map, where key - course and value - map with average grade and attempt."""
+        return {
+            i['module_state_key']: {'grade_avg': i['grade_avg'], 'attempts_avg': i['attempts_avg']}
+            for i in cls.academic_performance_request(course_key)
+        }
+
+    def get_homework_stat(self, course_key):
+        """
+        Provide statistic for given course.
+
+        :param course_key:  object, that represent course.
+        :return: map with list of correct answers, attempts, list of problems for unit and names.
+        Each item of given list represent one unit.
+        """
+        academic_performance = ProblemHomeWorkStatisticView.get_academic_performance(course_key)
+        course = get_course_by_id(course_key, depth=4)
+        stat = {'correct_answer': [], 'attempts': [], 'problems': [], 'names': []}
+        hw_number = 0
+
+        for subsection in chain.from_iterable(section.get_children() for section in course.get_children()):
+            hw_number += 1
+            stat['correct_answer'].append(0)
+            stat['attempts'].append(0)
+            stat['problems'].append([])
+            stat['names'].append('HW{}'.format(hw_number))
+            problems_in_hw = 0
+
+            for child in chain.from_iterable(unit.get_children() for unit in subsection.get_children()):
+                if child.location.category == 'problem':
+                    problem_id = child.location.to_deprecated_string()
+                    if problem_id in academic_performance:
+                        current_performance = academic_performance[problem_id]
+                        stat['correct_answer'][-1] += current_performance['grade_avg']
+                        stat['attempts'][-1] += current_performance['attempts_avg']
+                        problems_in_hw += 1
+
+                    stat['problems'][-1].append(problem_id)
+
+            if problems_in_hw > 0:
+                stat['correct_answer'][-1] /= problems_in_hw
+                stat['attempts'][-1] /= problems_in_hw
+
+        return stat
+
+    def post(self, request, course_id):
+        """Process post request."""
+        try:
+            course_key = CourseKey.from_string(course_id)
+        except InvalidKeyError:
+            log.error(u"Unable to find course with course key %s while getting enrollment statistic", course_id)
+            return HttpResponseBadRequest()
+
+        if not has_access(request.user, 'staff', course_key):
+            log.error("Problem homework statistics not available for user type `%s`", request.user)
+            return HttpResponseForbidden()
+
+        stat = self.get_homework_stat(course_key)
+
+        return JsonResponse(data=stat)
+
+
+class ProblemsStatisticView(View):
+    """Api for getting statistic for each problem in unit."""
+
+    def post(self, request, course_id):
+        """Process post request."""
+        try:
+            course_key = CourseKey.from_string(course_id)
+        except InvalidKeyError:
+            log.error(u"Unable to find course with course key %s while getting enrollment statistic", course_id)
+            return HttpResponseBadRequest()
+
+        if not has_access(request.user, 'staff', course_key):
+            log.error("Problem`s statistics not available for user type `%s`", request.user)
+            return HttpResponseForbidden()
+
+        problems = [course_key.make_usage_key_from_deprecated_string(p) for p in request.POST.getlist('problems')]
+        stats = (StudentModule.objects
+                              .filter(module_state_key__in=problems)
+                              .values('module_state_key')
+                              .annotate(correct=Sum('grade'))
+                              .annotate(incorrect=Sum('grade') - Sum('max_grade'))
+                              .values('module_state_key', 'correct', 'incorrect'))
+        incorrect, correct = tuple(map(list, zip(*[(int(s['incorrect'] or 0), int(s['correct'] or 0)) for s in stats])))
+        return JsonResponse(data={'incorrect': incorrect, 'correct': correct})
+
+
+class ProblemDetailView(View):
+    """Api for getting problem detail."""
+
+    def post(self, request, course_id):
+        """Process post request."""
+        try:
+            course_key = CourseKey.from_string(course_id)
+        except InvalidKeyError:
+            log.error(u"Unable to find course with course key %s while getting enrollment statistic", course_id)
+            return HttpResponseBadRequest()
+
+        if not has_access(request.user, 'staff', course_key):
+            log.error("Problem detail not available for user type `%s`", request.user)
+            return HttpResponseForbidden()
+
+        result = xblock_view(request, course_id, request.POST['problem'], 'student_view')
+        return result
+
+
+class ProblemQuestionParser:
+    """Base class for provide statistic for question."""
+
+    __metaclass__ = ABCMeta
+
+    def __init__(self, problemID, questionID, answer_map):
+        """
+        Constructor.
+
+        :param problemID: object (not string), that represent problem ID
+        :param questionID: string that represent question
+        :param answer_map: map for link data from data base to real data
+        """
+        self.answer_map = answer_map
+        self.problemID = problemID
+        self.questionID = questionID
+
+    @staticmethod
+    def init_statistic_object():
+        """Provide init state for statistic."""
+        return {}
+
+    @abstractmethod
+    def process_statistic_item(self, state, item):
+        """Abstract method for process data form database and update statistic."""
+        pass
+
+    def get_statistic(self):
+        """Provide statistic for given question."""
+        problems = StudentModule.objects.filter(module_state_key=self.problemID, grade__isnull=False,
+                                                module_type__exact="problem").values_list('state', flat=True)
+        result = self.init_statistic_object()
+        for p in problems:
+            self.process_statistic_item(result, json.loads(p))
+        return result
+
+
+class ProblemSelectQuestion(ProblemQuestionParser):
+    """Class for process question with `select` type (dropdown and single choice)."""
+
+    def __init__(self, problemID, questionID, answer_map):
+        """Implement constructor."""
+        super(ProblemSelectQuestion, self).__init__(problemID, questionID, answer_map)
+
+    def init_statistic_object(self):
+        """Overwrite base class."""
+        return {'type': 'bar', 'stats': {v: 0 for v in self.answer_map.values()}}
+
+    def process_statistic_item(self, state, item):
+        """Overwrite base class."""
+        state['stats'][self.answer_map[item['student_answers'][self.questionID]]] += 1
+
+
+class ProblemMultiSelectQuestion(ProblemSelectQuestion):
+    """Class for process question with `multySelect` type (question with checkboxes)."""
+
+    def __init__(self, problemID, questionID, answer_map):
+        """Implement constructor."""
+        super(ProblemMultiSelectQuestion, self).__init__(problemID, questionID, answer_map)
+
+    def process_statistic_item(self, state, item):
+        """Overwrite base class."""
+        for answer in item['student_answers'][self.questionID]:
+            state['stats'][self.answer_map[answer]] += 1
+
+
+class ProblemQuestionView(View):
+    """Api for question statistic."""
+
+    def post(self, request, course_id):
+        """Process post request."""
+        try:
+            course_key = CourseKey.from_string(course_id)
+        except InvalidKeyError:
+            log.error(u"Unable to find course with course key %s while getting enrollment statistic", course_id)
+            return HttpResponseBadRequest()
+
+        if not has_access(request.user, 'staff', course_key):
+            log.error("Question statistic not available for user type `%s`", request.user)
+            return HttpResponseForbidden()
+
+        type = request.POST['type']
+        questionID = request.POST['questionID']
+        answer_map = json.loads(request.POST['answerMap'])
+        problemID = course_key.make_usage_key_from_deprecated_string(request.POST['problemID'])
+
+        if type == QUESTUIN_SELECT_TYPE:
+            result = ProblemSelectQuestion(problemID, questionID, answer_map).get_statistic()
+        elif type == QUESTUIN_MULTI_SELECT_TYPE:
+            result = ProblemMultiSelectQuestion(problemID, questionID, answer_map).get_statistic()
+        else:
+            result = {}
+
+        return JsonResponse(data=result)
 
 
 class InstructorAnalyticsFragmentView(EdxFragmentView):
