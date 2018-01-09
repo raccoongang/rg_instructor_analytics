b69708599a948488f56dd9a504aeb6ac199607e8
Yura Braiko <yura.braiko@raccoongang.com>
2018-01-09 19:50:05 +0200

    Fix for ficus

--- rg_instructor_analytics/plugins.py
+++ rg_instructor_analytics/plugins.py
@@ -2,12 +2,13 @@
 Module for tabs description.
 """
 from courseware.access import has_access
-from courseware.tabs import EnrolledTab
+from courseware.tabs import CourseTab
 from django.utils.translation import ugettext_noop
-from xmodule.tabs import TabFragmentViewMixin
+from django.core.files.storage import get_storage_class
 
 
-class InstructorAnalyticsDashboardTab(TabFragmentViewMixin, EnrolledTab):
+
+class InstructorAnalyticsDashboardTab(CourseTab):
     """
     Provides information for tab.
     """
@@ -18,6 +19,8 @@ class InstructorAnalyticsDashboardTab(TabFragmentViewMixin, EnrolledTab):
     body_class = "instructor-analytics-tab"
     is_dynamic = True
     fragment_view_name = 'rg_instructor_analytics.views.InstructorAnalyticsFragmentView'
+    view_name = 'instructor_analytics_dashboard'
+
 
     @classmethod
     def is_enabled(cls, course, user=None):
@@ -25,3 +28,30 @@ class InstructorAnalyticsDashboardTab(TabFragmentViewMixin, EnrolledTab):
         Return true if the specified user has staff access.
         """
         return bool(user and has_access(user, 'staff', course, course.id))
+
+    def __init__(self, tab_dict):
+        super(InstructorAnalyticsDashboardTab, self).__init__(tab_dict)
+        self._fragment_view = None
+
+    @property
+    def url_slug(self):
+        """
+        Returns the slug to be included in this tab's URL.
+        """
+        return "tab/" + self.type
+
+    @property
+    def fragment_view(self):
+        """
+        Returns the view that will be used to render the fragment.
+        """
+        if not self._fragment_view:
+            self._fragment_view = get_storage_class(self.fragment_view_name)()
+        return self._fragment_view
+
+    def render_to_fragment(self, request, course, **kwargs):
+        """
+        Renders this tab to a web fragment.
+        """
+        return self.fragment_view.render_to_fragment(request, course_id=unicode(course.id), **kwargs)
+
--- rg_instructor_analytics/views.py
+++ rg_instructor_analytics/views.py
@@ -23,10 +23,16 @@ from django.http.response import HttpResponseForbidden, JsonResponse
 from django.utils.translation import ugettext as _
 from django.views.generic import View
 from edxmako.shortcuts import render_to_string
+
+try:
+    from lms.djangoapps.grades.new.course_grade_factory import CourseGradeFactory
+except:
+    from lms.djangoapps.grades.new.course_grade import CourseGradeFactory
+
 from lms.djangoapps.grades.new.course_grade_factory import CourseGradeFactory
 from opaque_keys import InvalidKeyError
 from opaque_keys.edx.keys import CourseKey
-from openedx.core.djangoapps.plugin_api.views import EdxFragmentView
+from web_fragments.views import FragmentView
 from student.models import CourseEnrollment
 from web_fragments.fragment import Fragment
 from xmodule.modulestore.django import modulestore
@@ -488,7 +494,7 @@ class GradebookView(AccessMixin, View):
         return JsonResponse(data={'student_info': student_info, 'exam_names': exam_names})
 
 
-class InstructorAnalyticsFragmentView(AccessMixin, EdxFragmentView):
+class InstructorAnalyticsFragmentView(AccessMixin, FragmentView):
     """
     Fragment for render tab.
     """
