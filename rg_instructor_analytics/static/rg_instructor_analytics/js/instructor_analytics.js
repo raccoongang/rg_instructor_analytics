399b4c96651a7f58561072f03bbb70fe23874b6d
Yura [flying_pi] Braiko <fp.flyingpi@gmail.com>
2018-02-02 17:29:21 +0200

    Add new tab for cohort (#12)
     Add new tab for cahort

--- rg_instructor_analytics/static/rg_instructor_analytics/js/instructor_analytics.js
+++ rg_instructor_analytics/static/rg_instructor_analytics/js/instructor_analytics.js
@@ -677,6 +677,102 @@
         return greadebookTab;
     }
 
+    function CohortTab(button, content) {
+        var cohortTab = new Tab(button, content);
+
+        cohortTab.cohortList = content.find('#cohort-check-list');
+        cohortTab.emailBody = cohortTab.content.find('#email-body');
+        cohortTab.emailBody.froalaEditor();
+
+        content.find('#cohort-send-email-btn').click(function () {
+            var ids = '';
+            $("input:checkbox[name=cohort-checkbox]:checked").each(function () {
+                if (ids.length > 0) {
+                    ids += ',';
+                }
+                ids += $(this).val();
+            });
+
+            var request = {
+                users_ids: ids,
+                subject: $('#email-subject').val(),
+                body: $('#email-body').froalaEditor('html.get'),
+            };
+
+            $.ajax({
+                traditional: true,
+                type: "POST",
+                url: "api/cohort/send_email/",
+                data: request,
+                success: () => console.log('Emails are sended'),
+                error:  () => console.log('Email sending failed'),
+                dataType: "json"
+            });
+        });
+
+        function updateCohort() {
+            function onSuccess(response) {
+                var plot = {
+                    y: response.values,
+                    x: response.labels,
+                    type: 'bar',
+                };
+                var data = [plot];
+                Plotly.newPlot('cohort-plot', data, {}, {displayModeBar: false});
+
+                cohortTab.cohortList.empty();
+                for(var i=0;i< response.cohorts.length;i++){
+                    cohortTab.cohortList.append(
+                        '<li>' +
+                            '<div>'+
+                                '<input name="cohort-checkbox" type="checkbox" value="'+
+                                    response.cohorts[i].students_id+'">'+
+                                '<label style="display: inline-block;">'+
+                                    '&ensp;'+response.labels[i]+':&ensp;'+response.cohorts[i].students_username+
+                                '</label>'+
+                            '</div>'+
+                        '</li>'
+                    )
+                }
+            }
+
+            function onError() {
+                alert("Statistics for the select course cannot be loaded.");
+            }
+
+            $.ajax({
+                traditional: true,
+                type: "POST",
+                url: "api/cohort/",
+                success: onSuccess,
+                error: onError,
+                dataType: "json"
+            });
+
+        }
+        cohortTab.loadTabData = updateCohort;
+        cohortTab.emailBody.on('froalaEditor.image.beforeUpload', function (e, editor, files) {
+            if (files.length) {
+                // Create a File Reader.
+                var reader = new FileReader();
+
+                // Set the reader to insert images when they are loaded.
+                reader.onload = function (e) {
+                    var result = e.target.result;
+                    editor.image.insert(result, null, null, editor.image.get());
+                };
+
+                // Read image as base64.
+                reader.readAsDataURL(files[0]);
+            }
+
+            // Stop default upload chain.
+            return false;
+        });
+
+        return cohortTab;
+    }
+
     function toggleToTab(tab) {
         tabList.forEach(function (t) {
             t.setActive(t === tab)
@@ -694,7 +790,10 @@
                 $content.find('#section-problem')),
             GradebookTab(
                 $content.find('#gradebook-btn'),
-                $content.find('#section-gradebook'))
+                $content.find('#section-gradebook')),
+            CohortTab(
+                $content.find('#cohort-btn'),
+                $content.find('#section-cohort'))
         ];
         tabList.forEach(function (tab) {
             tab.button.click(function () {
--- rg_instructor_analytics/urls.py
+++ rg_instructor_analytics/urls.py
@@ -4,8 +4,8 @@ Url config file.
 from django.conf.urls import url
 
 from rg_instructor_analytics.views import (
-    EnrollmentStatisticView, GradebookView, InstructorAnalyticsFragmentView, ProblemDetailView,
-    ProblemHomeWorkStatisticView, ProblemQuestionView, ProblemsStatisticView
+    CohortSendMessage, CohortView, EnrollmentStatisticView, GradebookView, InstructorAnalyticsFragmentView,
+    ProblemDetailView, ProblemHomeWorkStatisticView, ProblemQuestionView, ProblemsStatisticView
 )
 
 urlpatterns = [
@@ -24,6 +24,10 @@ urlpatterns = [
     url(r'^api/problem_statics/problem_question_stat/$', ProblemQuestionView.as_view(), name='problem_question_view'),
 
     url(r'^api/gradebook/$', GradebookView.as_view(), name='gradebook_view'),
+
+    url(r'^api/cohort/$', CohortView.as_view(), name='cohort_view'),
+
+    url(r'^api/cohort/send_email/$', CohortSendMessage.as_view(), name='send_email_to_cohort'),
 
     url(r'^', InstructorAnalyticsFragmentView.as_view(), name='instructor_analytics_dashboard'),
 ]
