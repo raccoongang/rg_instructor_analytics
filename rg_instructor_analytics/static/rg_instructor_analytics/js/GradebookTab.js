e6f1ceaec8282c48818451f0999ea5d5254a9a2e
Misha <suxx_2000@hotmail.com>
2018-05-02 17:51:13 +0300

    init

--- rg_instructor_analytics/static/rg_instructor_analytics/js/GradebookTab.js
+++ rg_instructor_analytics/static/rg_instructor_analytics/js/GradebookTab.js
@@ -86,7 +86,7 @@ function GradebookTab(button, content) {
         });
 
         $tableCells.each((item) => {
-            $tableCells[item].style.width = maxLength;
+            $tableCells[item].style.flex = `0 0 ${maxLength}px`;
         });
 
         $(greadebookTab.gradebookTableBody).click(function (element) {
@@ -110,7 +110,8 @@ function GradebookTab(button, content) {
 
             var layout = {
                 title: greadebookTab.studentInfo[element.target.dataset['position']].username,
-                showlegend: false
+                showlegend: false,
+                xaxis: {domain: [0, 0.97]},
             };
             $('.gradebook-table-row').removeClass('active');
             $(element.target).closest('.gradebook-table-row').toggleClass('active');
