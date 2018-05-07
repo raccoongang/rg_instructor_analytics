a70af935bd78a24d36b96ffcca75a83e86b323fe
Yura Braiko <yura.braiko@raccoongang.com>
2018-05-07 11:42:56 +0300

    some design fixes

--- rg_instructor_analytics/static/rg_instructor_analytics/js/ProblemTab.js
+++ rg_instructor_analytics/static/rg_instructor_analytics/js/ProblemTab.js
@@ -79,6 +79,7 @@ function ProblemTab(button, content) {
 
             $homework.html(bars);
 
+            $homework.unbind( "click" );
             $homework.on('click', function (e) {
                 $('.enrollment-title-1.hidden, .enrollment-title-text-1.hidden,  .enrollment-legend-holder__square-1').removeClass('hidden');
                 if ($(e.target).closest('li').data()) {
