f16fbb19c7675fb6848b5d0b36d88472a3306ec8
Yura Braiko <yura.braiko@raccoongang.com>
2018-05-22 19:26:53 +0300

    Style suggestion

--- rg_instructor_analytics/static/rg_instructor_analytics/js/Suggestion.js
+++ rg_instructor_analytics/static/rg_instructor_analytics/js/Suggestion.js
@@ -5,7 +5,11 @@ function SuggestionTab(button, content) {
     suggestionTab.loadTabData = () => {
         let onSuggestionLoad =  (response) => {
             const suggestionContent = suggestionTab.content.find('#suggestion-content');
-            suggestionContent[0].innerHTML = response.suggestion.map(renderSuggestion).join('\n');
+            if(response.suggestion.length > 0) {
+                suggestionContent[0].innerHTML = response.suggestion.map(renderSuggestion).join('\n');
+            }else{
+                suggestionContent[0].innerHTML = '<div>No suggestions so far</div>'
+            }
             suggestionContent.find('.go-to-item').click((e) => {
                 const location = JSON.parse(e.target.dataset.location);
                 suggestionTab.tabHolder.openLocation(location);
@@ -15,10 +19,14 @@ function SuggestionTab(button, content) {
         let renderSuggestion = (suggestion) => {
             return (
                 `<div class="suggestion-item">
-                    <div class="desctiption">
+                    <div class="desctiption suggestion-message">
                         ${suggestion.description}
                     </div>
-                    <button class="go-to-item" data-location='${JSON.stringify(suggestion.location)}'>Go to item</button>
+                    <button 
+                        class="go-to-item suggestion-button" 
+                        data-location='${JSON.stringify(suggestion.location)}'>
+                            Go to item
+                    </button>
                 </div>`
             )
         };
