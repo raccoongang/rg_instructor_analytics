e8f727aae017cd7438196d326cd1e17f5a175290
wowkalucky <wowkalucky@gmail.com>
2018-10-02 20:23:35 +0300

    Remove AccessMixin from project

--- rg_instructor_analytics/utils/decorators.py
+++ rg_instructor_analytics/utils/decorators.py
@@ -6,6 +6,7 @@ import logging
 
 from django.http import HttpResponseBadRequest, HttpResponseForbidden
 from django.utils.decorators import available_attrs
+from django.utils.translation import ugettext as _
 from opaque_keys import InvalidKeyError
 from opaque_keys.edx.keys import CourseKey
 
@@ -26,7 +27,7 @@ def instructor_access_required(view_func):
 
     @wraps(view_func, assigned=available_attrs(view_func))
     def _wrapped_view(request, *args, **kwargs):
-        course_id = request.POST.get('course_id')
+        course_id = request.POST.get('course_id') or kwargs.get('course_id')
         try:
             course_key = CourseKey.from_string(course_id)
             course = get_course(course_key)
@@ -34,10 +35,13 @@ def instructor_access_required(view_func):
                 return HttpResponseForbidden(reason='Instructors only!')
             return view_func(request, *args, **kwargs)
 
-        except (ValueError, InvalidKeyError) as exc:
+        except InvalidKeyError:
             log.error(
                 "Unable to find course with course key %s while loading the Instructor Analytics dashboard.",
                 course_id
             )
+            return HttpResponseBadRequest(reason=_("Invalid course ID."))
+        except ValueError as exc:
+            log.error(exc.message, course_id)
             return HttpResponseBadRequest(reason=exc.message)
     return _wrapped_view
--- rg_instructor_analytics/views/tab_fragment.py
+++ rg_instructor_analytics/views/tab_fragment.py
@@ -5,8 +5,11 @@ import json
 import sys
 from time import mktime
 
-from django.conf import settings
-from django.http import Http404
+from django.http import Http404, HttpResponseBadRequest
+from django.utils.decorators import method_decorator
+from django.utils.translation import ugettext as _
+from opaque_keys import InvalidKeyError
+from opaque_keys.edx.keys import CourseKey
 from web_fragments.fragment import Fragment
 from web_fragments.views import FragmentView
 
@@ -14,7 +17,7 @@ from courseware.courses import get_course_by_id
 from edxmako.shortcuts import render_to_string
 from openedx.core.djangoapps.content.course_overviews.models import CourseOverview
 from rg_instructor_analytics.utils import resource_string
-from rg_instructor_analytics.utils.AccessMixin import AccessMixin
+from rg_instructor_analytics.utils.decorators import instructor_access_required
 from student.models import CourseAccessRole
 
 # NOTE(flying-pi) reload(sys) is used for restore method `setdefaultencoding`,
@@ -22,72 +25,36 @@ from student.models import CourseAccessRole
 reload(sys)
 sys.setdefaultencoding('utf8')
 
-JS_URL = '{static_url}rg_instructor_analytics/js/'.format(static_url=settings.STATIC_URL)
-CSS_URL = '{static_url}rg_instructor_analytics/css/'.format(static_url=settings.STATIC_URL)
 
-
-class InstructorAnalyticsFragmentView(AccessMixin, FragmentView):
+class InstructorAnalyticsFragmentView(FragmentView):
     """
     Fragment for render tab.
     """
 
-    def get_enroll_info(self, course):
+    @method_decorator(instructor_access_required)
+    def dispatch(self, *args, **kwargs):
         """
-        Return enroll_start and enroll_end for given course.
+        See: https://docs.djangoproject.com/en/1.8/topics/class-based-views/intro/#id2.
         """
-        enroll_start = course.enrollment_start
-        if enroll_start is None:
-            enroll_start = course.start
+        return super(InstructorAnalyticsFragmentView, self).dispatch(*args, **kwargs)
 
-        enroll_end = course.enrollment_end
-        if enroll_end is None:
-            enroll_end = course.end
-
-        return {
-            'enroll_start': mktime(enroll_start.timetuple()) if enroll_start else 'null',
-            'enroll_end': mktime(enroll_end.timetuple()) if enroll_end else 'null',
-        }
-
-    def get_available_courses(self, user):
-        """
-        Return courses, available for the given user.
-        """
-        result = []
-        # For staff user we need return all available courses on platform.
-        if user.is_staff:
-            available_courses = CourseOverview.objects.all()
-            for course in available_courses:
-                try:
-                    result.append(get_course_by_id(course.id, depth=0))
-                except Http404:
-                    continue
-        # Return courses, where user has permission as instructor of staff
-        else:
-            available_courses = CourseAccessRole.objects.filter(user=user, role__in=['instructor', 'staff'])
-            exist_courses_id = []
-            for record in available_courses:
-                try:
-                    course = get_course_by_id(record.course_id, depth=0)
-                    course_id = str(course.id)
-                    if course_id not in exist_courses_id:
-                        result.append(course)
-                        exist_courses_id.append(course_id)
-                except Http404:
-                    continue
-        return result
-
-    def process(self, request, **kwargs):
+    def render_to_fragment(self, request, *args, **kwargs):
         """
         Render tab fragment.
         """
-        course = kwargs['course']
+        try:
+            course_key = CourseKey.from_string(kwargs.get('course_id'))
+            course = get_course_by_id(course_key)
+        except InvalidKeyError:
+            return HttpResponseBadRequest(_("Invalid course ID."))
+
         available_courses = [
             {
-                'course_id': str(c.id),
-                'course_name': str(c.display_name),
-                'is_current': course == c,
+                'course_id': str(user_course.id),
+                'course_name': str(user_course.display_name),
+                'is_current': course == user_course,
             }
-            for c in self.get_available_courses(request.user)
+            for user_course in self.get_available_courses(request.user)
         ]
 
         enroll_info = {
@@ -117,3 +84,50 @@ class InstructorAnalyticsFragmentView(AccessMixin, FragmentView):
         fragment.add_javascript(resource_string("js/base.js"))
 
         return fragment
+
+    @staticmethod
+    def get_enroll_info(course):
+        """
+        Return enroll_start and enroll_end for given course.
+        """
+        enroll_start = course.enrollment_start
+        if enroll_start is None:
+            enroll_start = course.start
+
+        enroll_end = course.enrollment_end
+        if enroll_end is None:
+            enroll_end = course.end
+
+        return {
+            'enroll_start': mktime(enroll_start.timetuple()) if enroll_start else 'null',
+            'enroll_end': mktime(enroll_end.timetuple()) if enroll_end else 'null',
+        }
+
+    @staticmethod
+    def get_available_courses(user):
+        """
+        Return courses, available for the given user.
+        """
+        result = []
+        # For staff user we need return all available courses on platform.
+        if user.is_staff:
+            available_courses = CourseOverview.objects.all()
+            for course in available_courses:
+                try:
+                    result.append(get_course_by_id(course.id, depth=0))
+                except Http404:
+                    continue
+        # Return courses, where user has permission as instructor of staff
+        else:
+            available_courses = CourseAccessRole.objects.filter(user=user, role__in=['instructor', 'staff'])
+            exist_courses_id = []
+            for record in available_courses:
+                try:
+                    course = get_course_by_id(record.course_id, depth=0)
+                    course_id = str(course.id)
+                    if course_id not in exist_courses_id:
+                        result.append(course)
+                        exist_courses_id.append(course_id)
+                except Http404:
+                    continue
+        return result
