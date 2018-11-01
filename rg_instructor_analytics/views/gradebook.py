51f64e8539d08e820a462b53b4b4281e04660c09
OlhaShyliaieva <olshilyaeva@gmail.com>
2018-11-01 15:58:29 +0200

    Fix import

--- rg_instructor_analytics/views/gradebook.py
+++ rg_instructor_analytics/views/gradebook.py
@@ -12,15 +12,12 @@ from django.utils.translation import ugettext as _
 from django.views.generic import View
 from opaque_keys import InvalidKeyError
 from opaque_keys.edx.keys import CourseKey
-
 from rg_instructor_analytics_log_collector.models import DiscussionActivity, VideoViewsByUser
 
 import django_comment_client.utils as utils
 from lms.djangoapps.courseware.courses import get_course_by_id
-
 from rg_instructor_analytics.models import GradeStatistic
 from rg_instructor_analytics.utils.decorators import instructor_access_required
-
 
 
 class GradebookView(View):
