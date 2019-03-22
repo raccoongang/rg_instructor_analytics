2ec0d1ef6f589fea6d9f647615661cc4ea9cfc51
Iana Vasylieva <ianavasylieva@Ianas-MacBook-Pro.local>
2019-03-22 13:03:58 +0200

    Make docstrings

--- rg_instructor_analytics/utils/ginkgo_ficus_specific.py
+++ rg_instructor_analytics/utils/ginkgo_ficus_specific.py
@@ -1,33 +1,47 @@
+"""
+Helper file for inter-version compatibility.
+
+This file and its sibling hawthorn_specific.py
+serve for compatibility of rg_instructor_analytics app with both
+ginkgo and hawthorn releases.
+The following methods convert their arguments to make them usable in
+ginkgo and ficus releases. When the app is installed to ginkgo or ficus release,
+this file is imported wherever there is version conflict.
+
+The major changes between ginkgo and hawthorn releases, which affect the app are:
+ - CourseEnrollment model: course_id changed from CourseKeyField to property
+   that returns CourseKey object
+ - StudentModule model: module_state_key was changed from LocationKeyField to UsageKeyField
+"""
+
 from opaque_keys.edx.keys import CourseKey
 
 
 def get_problem_id(xblock):
     """
-    rg_instructor_analytics/views/problem.py: line 123
-    problem_id = child.location.to_deprecated_string()
+    Convert xblock's location to string.
+
+    :param xblock: problem
+    :return: problem Locator object
     """
     return xblock.location.to_deprecated_string()
 
 
 def get_problem_str(problem_id):
     """
-    rg_instructor_analytics/views/problem.py: line 129
-    stats['problems'][-1].append(problem_id)
+    Return problem location string unchanged.
 
-    rg_instructor_analytics/views/funnel.py: line 151
-    if info['module_state_key'] not in result:
-        result[info['module_state_key']] = []
-    result[info['module_state_key']].append({ ...
+    :param problem_id: problem location string
+    :return: problem location string
     """
     return problem_id
 
 
 def get_course_key(course_id):
     """
-    rg_instructor_analytics/tasks.py: line 98
-    try:
-        course_key = CourseKey.from_string(item['course_id'])
-    except InvalidKeyError:
-        continue
+    Convert string to CourseKey object.
+
+    :param course_id: string
+    :return: CourseKey object
     """
     return CourseKey.from_string(course_id)
--- rg_instructor_analytics/utils/hawthorn_specific.py
+++ rg_instructor_analytics/utils/hawthorn_specific.py
@@ -1,30 +1,45 @@
+"""
+Helper file for inter-version compatibility.
+
+This file and its sibling ginkgo_ficus_specific.py
+serve for compatibility of rg_instructor_analytics app with both
+ginkgo and hawthorn releases.
+The following methods convert their arguments to make them usable in hawthorn release.
+When the app is installed to hawthorn release, this file is imported wherever
+there is version conflict.
+
+The major changes between ginkgo and hawthorn releases, which affect the app are:
+ - CourseEnrollment model: course_id changed from CourseKeyField to property
+   that returns CourseKey object
+ - StudentModule model: module_state_key was changed from LocationKeyField to UsageKeyField
+"""
+
+
 def get_problem_id(xblock):
     """
-    rg_instructor_analytics/views/problem.py: line 123
-    problem_id = child.location # hawthorn
+    Get Locator object for xblock.
+
+    :param xblock: problem
+    :return: Locator
     """
     return xblock.location
 
 
 def get_problem_str(problem_id):
     """
-    rg_instructor_analytics/views/problem.py: line 129
-    stats['problems'][-1].append(problem_id.to_deprecated_string())
+    Get string form problem's Location.
 
-    rg_instructor_analytics/views/funnel.py: line 151
-    if info['module_state_key'].to_deprecated_string() not in result:
-        result[info['module_state_key'].to_deprecated_string()] = []
-    result[info['module_state_key'].to_deprecated_string()].append({  # hawthorn
+    :param problem_id: Locator
+    :return: string
     """
     return problem_id.to_deprecated_string()
 
 
 def get_course_key(course_id):
     """
-    rg_instructor_analytics/tasks.py: line 98
-    try:
-        course_key = item['course_id']
-    except InvalidKeyError:
-        continue
+    Return CourseKey object without changes.
+
+    :param course_id: CourseKey object
+    :return: CourseKey object
     """
     return course_id
