a770508e42f30c8aea35148511654b1227ff10e8
Iana <33125830+noisysky@users.noreply.github.com>
2019-06-13 12:35:11 +0300

    Remove duplicated meta information (#135)
    Rewrite Analytics fragment view as a regular function-based view

--- rg_instructor_analytics/urls.py
+++ rg_instructor_analytics/urls.py
@@ -11,7 +11,7 @@ from rg_instructor_analytics.views.problem import (
     ProblemDetailView, ProblemHomeWorkStatisticView, ProblemQuestionView, ProblemsStatisticView
 )
 from rg_instructor_analytics.views.suggestion import SuggestionView
-from rg_instructor_analytics.views.tab_fragment import InstructorAnalyticsFragmentView
+from rg_instructor_analytics.views.tab_fragment import instructor_analytics_dashboard
 
 urlpatterns = [
     # Enrollment stats tab:
@@ -52,5 +52,5 @@ urlpatterns = [
     # Suggestions tab:
     url(r'^api/suggestion/$', SuggestionView.as_view(), name='suggestion'),
 
-    url(r'^', InstructorAnalyticsFragmentView.as_view(), name='instructor_analytics_dashboard'),
+    url(r'^$', instructor_analytics_dashboard, name='instructor_analytics_dashboard'),
 ]
