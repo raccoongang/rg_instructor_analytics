c8af0341acd656eb573acc6c8a98b3f65e93e158
Olha <olha.kopylova@raccoongang.com>
2019-11-21 15:49:32 +0000

    [RGA2-116] Optimization of 'Transition Pattern through Units' diagram Change multiple reequests to StudentStepCourse for count every unit id to one request with 'group by' by unit id

--- rg_instructor_analytics/views/activity.py
+++ rg_instructor_analytics/views/activity.py
@@ -3,6 +3,7 @@ Gradebook sub-tab module.
 """
 from datetime import datetime, timedelta
 
+from django.db.models import Count
 from django.http import HttpResponseBadRequest, JsonResponse
 from django.utils.decorators import method_decorator
 from django.utils.translation import ugettext as _
@@ -102,30 +103,48 @@ class ActivityView(View):
         Get statistic of visiting units.
         """
         course = get_course_by_id(course_key, depth=3)
+
         ticktext = []
         tickvals = []
         count_visits = []
 
+        def _make_query(field_name):
+            """
+            Create aggregated query and return it's result
+
+            Query format is:
+                SELECT <field_name>, COUNT(<field_name>) AS <field_name>__count
+                FROM rg_instructor_analytics_log_collector_studentstepcourse
+                WHERE (course = '<course_key>' AND log_time BETWEEN '<start_date>' AND '<end_date>')
+                GROUP BY <field_name>
+
+            :param field_name: field that contain unit id, could be 'current_unit' or 'target_unit'
+            :return: dict, {<unit_id>: <count>}
+            """
+            qs = StudentStepCourse.objects.filter(
+                course=course_key,
+                log_time__range=(from_date, to_date + timedelta(days=1))
+            )
+            qs = qs.values(field_name).annotate(Count(field_name))
+            return dict(qs.values_list(field_name, '{}__count'.format(field_name)))
+
+        # 'current_unit' mean user leave unit, 'target_unit' mean user come to unit
+        counted_current_units = _make_query('current_unit')
+        counted_target_units = _make_query('target_unit')
+
         for section in course.get_children():
             for subsection in section.get_children():
-                len_units = len(subsection.get_children())
-                for index, unit in enumerate(subsection.get_children(), start=1):
+                for unit in subsection.get_children():
                     tickvals.append(unit.location.block_id)
                     ticktext.append(unit.display_name)
-                    if len_units == index:
-                        count_visits.append(
-                            StudentStepCourse.objects.filter(
-                                current_unit=unit.location.block_id,
-                                log_time__range=(from_date, to_date + timedelta(days=1)),
-                            ).count()
+                    # we could have some missed events like 'Start Course' click or go to subsection from Course Outline
+                    # so why we select highest value from current_unit and target_unit counts
+                    count_visits.append(
+                        max(
+                            counted_current_units.get(unit.location.block_id, 0),
+                            counted_target_units.get(unit.location.block_id, 0)
                         )
-                    else:
-                        count_visits.append(
-                            StudentStepCourse.objects.filter(
-                                target_unit=unit.location.block_id,
-                                log_time__range=(from_date, to_date + timedelta(days=1)),
-                            ).count()
-                        )
+                    )
 
         return {
             'ticktext': ticktext,
